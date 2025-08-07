1279eb6a2424277f2f9138d11d7a1349fe93f8a2
Dan McCabe <danmccabe17@gmail.com>
2025-08-07 14:18:08 -0700

    update __init__ to simplify imports and define root location

--- nrel/routee/transit/__init__.py
+++ nrel/routee/transit/__init__.py
@@ -1,1 +1,15 @@
-# __init__.py for routee_transit
+from pathlib import Path
+
+__all__ = ["build_routee_features_with_osm", "predict_for_all_trips"]
+
+from .prediction.gtfs_feature_processing import build_routee_features_with_osm
+from .prediction.routee import predict_for_all_trips
+
+
+def package_root() -> Path:
+    """Return the path to the nrel.routee.powertrain package"""
+    return Path(__file__).parent
+
+def repo_root() -> Path:
+    """Return the path to the root directory of the repository"""
+    return Path(__file__).parent.parent.parent.parent
