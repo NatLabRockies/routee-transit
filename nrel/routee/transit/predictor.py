e9a3dbb59050b7baed1e6799984e54ee694f603b
Dan McCabe <danmccabe17@gmail.com>
2026-01-28 13:11:51 -0800

    format with ruff

--- nrel/routee/transit/predictor.py
+++ nrel/routee/transit/predictor.py
@@ -369,7 +369,7 @@ class GTFSEnergyPredictor:
             left_on="service_id",
             right_index=True,
         )
-        
+
         # Mark each of these original trips as service
         self.trips["trip_type"] = "service"
 
